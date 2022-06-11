8db99ebe8d60d265f5c6b1b6624bc539f7a2359e
Matdrox <Matdrox@users.noreply.github.com>
2022-06-11 19:09:28 +0200

    Added Grid System

--- src/App.js
+++ src/App.js
@@ -3,9 +3,12 @@ import Card from './components/Card';
 function App() {
   return (
     // PURPLE IF DARK MODE!
-    <div className="w-full h-screen bg-gradient-to-bl from-blue-50 to-blue-400">
-      {/* GLASS CARD WITH GOD'S STATUE */}
-      <Card/>
+    <div className='w-full min-h-screen flex items-center justify-center bg-gradient-to-bl from-blue-50 to-blue-400'>
+      The Greek Mythology Search Database
+      <div className='container grid grid-cols-6 gap-10'>
+        {/* GLASS CARD WITH GOD'S STATUE */}
+        <Card />
+      </div>
     </div>
   );
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 const Card = () => {
   return (
-    <div className='bg-black w-20 h-50'>
-      <h1 className='text-white'>ATHENA</h1>
-      <h2 className='text-white'>Αθηνα</h2>
+    <div className='bg-white/30 h-30 col-span-1 row-span-2 grid grid-cols-4 grid-rows-4 rounded-xl shadow-md'>
+      <h1 className='row-start-1 row-span-1 text-white text-3xl font-bold'>ATHENA</h1>
+      <h2 className='row-start-2 row-span-1 text-white text-3xl'>ΑΘΗΝΑ</h2>
     </div>
   );
 };
