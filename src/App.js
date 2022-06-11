39023c00820d966d662ff43eab1e52856f7b0108
Matdrox <Matdrox@users.noreply.github.com>
2022-06-11 18:27:38 +0200

    Added Components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
+import Card from './components/Card';
+
 function App() {
   return (
-    <div className="w-full h-screen bg-black">
-      <h1>HEJ</h1>
+    // PURPLE IF DARK MODE!
+    <div className="w-full h-screen bg-gradient-to-bl from-blue-50 to-blue-400">
+      {/* GLASS CARD WITH GOD'S STATUE */}
+      <Card/>
     </div>
   );
 }
---
+++ src/components/Card.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Card = () => {
+  return (
+    <div className='bg-black w-20 h-50'>
+      <h1 className='text-white'>ATHENA</h1>
+      <h2 className='text-white'>Αθηνα</h2>
+    </div>
+  );
+};
+
+export default Card;
