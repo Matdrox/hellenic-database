a67bb90d048d752c6b59292c7f47a34f4d6939ce
Matdrox <Matdrox@users.noreply.github.com>
2022-06-12 16:42:36 +0200

    Added God Information on Search

--- src/App.js
+++ src/App.js
@@ -1,49 +1,55 @@
 import { useState, useEffect } from 'react';
+import GreekMyth, { allCollection } from 'greek-mythology-data';
 import Card from './components/Card';
 import Search from './components/Search';
 
 function App() {
-	const [mythsList, setMythsList] = useState([]);
-	const [search, setSearch] = useState('');
+  const [mythsList, setMythsList] = useState([]);
+  const [search, setSearch] = useState('');
+  const cors = require('cors');
 
-	const HandleSearch = (e) => {
-		e.preventDefault();
-		FetchSearch(search);
-	};
+  const HandleSearch = (e) => {
+    e.preventDefault();
+    FindSearch(search);
+  };
 
-	const FetchSearch = async (query) => {
-		const temp = await fetch(
-			`https://anfi.tk/greekApi/person/en/${query}`
-		).then((res) => res.json());
-		console.log(temp.results);
-		setMythsList(temp.results);
-	};
+  const FindSearch = (search) => {
+		search = search[0].toUpperCase() + search.slice(1).toLowerCase();
+    const allGods = new GreekMyth(allCollection);
+		console.log(allGods.list[1].greekName.split(',')[0])
+		console.log(allGods.list.find(item => item.name === search))
+  };
 
-	return (
-		// PURPLE IF DARK MODE!
-		<div className='w-full min-h-screen flex flex-col items-center bg-gradient-to-bl from-blue-100 to-blue-400'>
-			<h1 className='text-white font-bold text-5xl text-center mt-20'>
-				Hellenic Database
-			</h1>
-			<p className='text-white text-3xl text-center'>
-				The Greek Mythology Search Database
-			</p>
-			<br />
-			<p className='text-white text-2xl text-center'>
-				Search for a Hellenic god or creature:
-			</p>
-			<Search
-				HandleSearch={HandleSearch}
-				search={search}
-				setSearch={setSearch}
-				mythsList={mythsList}
-			/>
-			<div className='container grid grid-cols-6 grid-rows-4 gap-10'>
-				{/* GLASS CARD WITH GOD'S STATUE */}
-				<Card />
-			</div>
-		</div>
-	);
+  return (
+    // PURPLE IF DARK MODE!
+    <div className='w-full min-h-screen flex flex-col items-center bg-gradient-to-bl from-blue-100 to-blue-400'>
+      <h1 className='text-white font-bold text-5xl text-center mt-20'>
+        Hellenic Database
+      </h1>
+      <p className='text-white text-3xl text-center'>
+        The Greek Mythology Search Database
+      </p>
+      <br />
+      <p className='text-white text-2xl text-center'>
+        Search for a Hellenic god or creature:
+      </p>
+      <Search
+        HandleSearch={HandleSearch}
+        search={search}
+        setSearch={setSearch}
+        mythsList={mythsList}
+      />
+      {/* <div className='container grid grid-cols-6 grid-rows-4 gap-10'> */}
+      <div className='p-10 w-full grid grid-cols-1 gap-5 lg:grid-cols-5'>
+        {/* GLASS CARD WITH GOD'S STATUE */}
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- src/components/Search.js
+++ src/components/Search.js
@@ -2,28 +2,28 @@ import React from 'react';
 import { FaSearch } from 'react-icons/fa';
 
 const Search = (props) => {
-	return (
-		<form
-			className='flex items-center shadow-md rounded'
-			onSubmit={props.HandleSearch}
-		>
-			<input
-				className='pl-2 rounded rounded-r-none p-2
+  return (
+    <form
+      className='flex items-center shadow-md rounded'
+      onSubmit={props.HandleSearch}
+    >
+      <input
+        className='pl-2 rounded rounded-r-none p-2
         focus:outline-none'
-				type='search'
-				placeholder='Zeus'
-				value={props.search}
-				onChange={(e) => props.setSearch(e.target.value)}
-				required
-			></input>
-			<button
-				type='submit'
-				className='bg-blue-400 w-10 h-10 rounded rounded-l-none flex items-center justify-center'
-			>
-				<FaSearch className='text-white text-lg' />
-			</button>
-		</form>
-	);
+        type='search'
+        placeholder='Zeus'
+        value={props.search}
+        onChange={(e) => props.setSearch(e.target.value)}
+        required
+      ></input>
+      <button
+        type='submit'
+        className='bg-blue-400 w-10 h-10 rounded rounded-l-none flex items-center justify-center'
+      >
+        <FaSearch className='text-white text-lg' />
+      </button>
+    </form>
+  );
 };
 
 export default Search;
