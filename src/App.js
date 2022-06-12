cdcbde5d4ce64b2f9d8735d57e82b85ed478b990
Matdrox <Matdrox@users.noreply.github.com>
2022-06-12 17:16:12 +0200

    Implemented Real Time Search

--- src/App.js
+++ src/App.js
@@ -4,20 +4,38 @@ import Card from './components/Card';
 import Search from './components/Search';
 
 function App() {
-  const [mythsList, setMythsList] = useState([]);
-  const [search, setSearch] = useState('');
+  // const [mythsList, setMythsList] = useState([]);
+  // const [search, setSearch] = useState('');
+  const [search, setSearch] = useState({
+    s: '',
+    results: [],
+    selected: {},
+  });
+
   const cors = require('cors');
 
   const HandleSearch = (e) => {
-    e.preventDefault();
-    FindSearch(search);
+    let s = e.target.value;
+    setSearch((prevSearch) => {
+      return { ...prevSearch, s: s };
+    });
+    // e.preventDefault();
+    // FindSearch(search);
+    console.log(search.s);
   };
 
-  const FindSearch = (search) => {
-		search = search[0].toUpperCase() + search.slice(1).toLowerCase();
+  const EnterSearch = (e) => {
+    if (e.key === 'Enter') {
+      FindSearch();
+    }
+  };
+
+  const FindSearch = (e) => {
+    search.s = search.s[0].toUpperCase() + search.s.slice(1).toLowerCase();
     const allGods = new GreekMyth(allCollection);
-		console.log(allGods.list[1].greekName.split(',')[0])
-		console.log(allGods.list.find(item => item.name === search))
+    console.log(allGods.list.find((item) => item.name === search.s));
+    // console.log(allGods.list[1].greekName.split(',')[0])
+    // console.log(search.s)
   };
 
   return (
@@ -37,7 +55,8 @@ function App() {
         HandleSearch={HandleSearch}
         search={search}
         setSearch={setSearch}
-        mythsList={mythsList}
+        FindSearch={FindSearch}
+        EnterSearch={EnterSearch}
       />
       {/* <div className='container grid grid-cols-6 grid-rows-4 gap-10'> */}
       <div className='p-10 w-full grid grid-cols-1 gap-5 lg:grid-cols-5'>
--- src/components/Search.js
+++ src/components/Search.js
@@ -3,26 +3,31 @@ import { FaSearch } from 'react-icons/fa';
 
 const Search = (props) => {
   return (
-    <form
+    <div
       className='flex items-center shadow-md rounded'
-      onSubmit={props.HandleSearch}
+      // onSubmit={console.log('few')}
+      // onSubmit={props.FindSearch}
+      // onSubmit={props.HandleSearch}
     >
       <input
         className='pl-2 rounded rounded-r-none p-2
         focus:outline-none'
         type='search'
         placeholder='Zeus'
-        value={props.search}
-        onChange={(e) => props.setSearch(e.target.value)}
+        // value={props.search}
+        // onChange={(e) => props.setSearch(e.target.value)}
+        onChange={props.HandleSearch}
+				onKeyPress={props.EnterSearch}
         required
       ></input>
       <button
         type='submit'
         className='bg-blue-400 w-10 h-10 rounded rounded-l-none flex items-center justify-center'
+				onClick={props.FindSearch}
       >
         <FaSearch className='text-white text-lg' />
       </button>
-    </form>
+    </div>
   );
 };
 
