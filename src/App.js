4751d6ab3f68b5c93f01ea46a422b2497f53c0bf
Matei Cananau <mateicananau@gmail.com>
2022-06-12 11:57:13 +0200

    Updated Responsive Design

--- src/App.js
+++ src/App.js
@@ -3,43 +3,47 @@ import Card from './components/Card';
 import Search from './components/Search';
 
 function App() {
-  const [mythsList, setMythsList] = useState([]);
-  const [search, setSearch] = useState('');
+	const [mythsList, setMythsList] = useState([]);
+	const [search, setSearch] = useState('');
 
-  const HandleSearch = (e) => {
-    e.preventDefault();
-    FetchSearch(search);
-  };
+	const HandleSearch = (e) => {
+		e.preventDefault();
+		FetchSearch(search);
+	};
 
-  const FetchSearch = async (query) => {
-    const temp = await fetch(
-      `https://anfi.tk/greekApi/person/en/${query}`
-    ).then((res) => res.json());
-    console.log(temp.results)
-    setMythsList(temp.results);
-  };
+	const FetchSearch = async (query) => {
+		const temp = await fetch(
+			`https://anfi.tk/greekApi/person/en/${query}`
+		).then((res) => res.json());
+		console.log(temp.results);
+		setMythsList(temp.results);
+	};
 
-  return (
-    // PURPLE IF DARK MODE!
-    <div className='w-full min-h-screen flex flex-col items-center justify-center bg-gradient-to-bl from-blue-100 to-blue-400'>
-      <h1 className='text-white font-bold text-5xl'>Hellenic Database</h1>
-      <p className='text-white text-3xl'>The Greek Mythology Search Database</p>
-      <br />
-      <p className='text-white text-2xl'>
-        Search for a Hellenic god or creature:
-      </p>
-      <Search
-        HandleSearch={HandleSearch}
-        search={search}
-        setSearch={setSearch}
-        mythsList={mythsList}
-      />
-      <div className='container grid grid-cols-6 gap-10'>
-        {/* GLASS CARD WITH GOD'S STATUE */}
-        <Card />
-      </div>
-    </div>
-  );
+	return (
+		// PURPLE IF DARK MODE!
+		<div className='w-full min-h-screen flex flex-col items-center bg-gradient-to-bl from-blue-100 to-blue-400'>
+			<h1 className='text-white font-bold text-5xl text-center mt-20'>
+				Hellenic Database
+			</h1>
+			<p className='text-white text-3xl text-center'>
+				The Greek Mythology Search Database
+			</p>
+			<br />
+			<p className='text-white text-2xl text-center'>
+				Search for a Hellenic god or creature:
+			</p>
+			<Search
+				HandleSearch={HandleSearch}
+				search={search}
+				setSearch={setSearch}
+				mythsList={mythsList}
+			/>
+			<div className='container grid grid-cols-6 grid-rows-4 gap-10'>
+				{/* GLASS CARD WITH GOD'S STATUE */}
+				<Card />
+			</div>
+		</div>
+	);
 }
 
 export default App;
--- src/components/Card.js
+++ src/components/Card.js
@@ -2,15 +2,15 @@ import React from 'react';
 import imgAthena from '../images/Athena.png';
 
 const Card = () => {
-  return (
-    <div className='bg-white/30 h-30 col-span-1 row-span-2 grid grid-cols-2 grid-rows-4 rounded-xl shadow-md'>
-      <h1 className='row-start-1 row-span-1 text-white text-3xl font-bold'>
-        ATHENA
-      </h1>
-      <h2 className='row-start-2 row-span-1 text-white text-3xl'>ΑΘΗΝΑ</h2>
-      <img className='row-start-1 col-start-2' src={imgAthena}/>
-    </div>
-  );
+	return (
+		<div className='bg-white/30 h-30 col-span-1 row-span-2 grid grid-cols-2 grid-rows-4 rounded-xl shadow-md'>
+			<h1 className='row-start-1 row-span-1 text-white text-3xl font-bold'>
+				ATHENA
+			</h1>
+			<h2 className='row-start-2 row-span-1 text-white text-3xl'>ΑΘΗΝΑ</h2>
+			<img className='row-start-1 col-start-2' src={imgAthena} />
+		</div>
+	);
 };
 
 export default Card;
