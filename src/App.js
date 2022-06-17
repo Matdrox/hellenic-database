e60908a6cb586d4725cd2ddc18ca132c330b2374
Matdrox <Matdrox@users.noreply.github.com>
2022-06-17 12:52:35 +0200

    Cleaned Up Code

--- src/App.js
+++ src/App.js
@@ -28,7 +28,6 @@ function App() {
 
   return (
     // PURPLE IF DARK MODE!
-    // <div className='w-full min-h-screen flex flex-col items-center bg-gradient-to-bl from-blue-100 to-blue-400'>
     <div className='w-full min-h-screen flex flex-col items-center bg-[url("images/background.jpg")] bg-cover'>
       <h1 className='text-white font-bold text-5xl text-center mt-20'>
         Hellenic Database
